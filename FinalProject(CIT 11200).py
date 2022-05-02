c92f176268979a6a561701f78918d76da2db5785
madthom <98051541+madthom@users.noreply.github.com>
2022-05-01 23:30:20 -0400

    Add files via upload

---
+++ FinalProject(CIT 11200).py
@@ -0,0 +1,76 @@
+"""
+Date: May 2022
+Purpose: To map content to careers in computing
+"""
+from tkinter import *
+
+# Window width and height
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 400
+
+<body>
+        <h1>Mapping Content To Careers</h1>
+        <h2>Digital Organization</h2>
+        <img scr="C:/Users/glenn/Downloads/CIT_11200/Program10Languages(zipped)/1.jpg" />
+        <p>
+            Utilizes digital tools or technology trends to its advantage both in its external and internal operations.
+	</p>
+	
+        <h2>Computer Hardware</h2>
+        <img scr="C:/Users/glenn/Downloads/CIT_11200/Program10Languages(zipped)/2.jpg" />
+        <p>
+            The physical components that a computer system requires to function.
+        </p>
+        
+        <h2>Numbers in Computing</h2>
+        <img scr="C:/Users/glenn/Downloads/CIT_11200/Program10Languages(zipped)/3.jpg" />
+        <p>
+            Format that's used for numeric values in digital device hardware and software.
+        </p>
+
+        <h2>The Internet</h2>
+        <img scr="C:/Users/glenn/Downloads/CIT_11200/Program10Languages(zipped)/4.jpg" />
+        <p>
+            Global network that uses protocols and data packets to exchange information.
+        </p>
+
+        <h2>Web Design</h2>
+        <img scr="C:/Users/glenn/Downloads/CIT_11200/Program10Languages(zipped)/5.jpg" />
+        <p>
+            Design of websites that are displayed on the internet.
+        </p>
+
+        <h2>Cybersercuity</h2>
+        <img scr="C:/Users/glenn/Downloads/CIT_11200/Program10Languages(zipped)/6.jpg" />
+        <p>
+            The protection of computer systems and the network from information disclosure, theft or damages to their hardware.
+        </p>
+
+        <h2>Legal Considerations in Computing</h2>
+        <img scr="C:/Users/glenn/Downloads/CIT_11200/Program10Languages(zipped)/7.jpg" />
+        <p>
+            Set of rules for computers and computer networks may be put for data privacy, etc.
+        </p>
+
+        <h2>Software</h2>
+        <img scr="C:/Users/glenn/Downloads/CIT_11200/Program10Languages(zipped)/8.jpg" />
+        <p>
+            Entire set of programs that are associated with the operations of a computer system.
+        </p>
+
+        <h2>Use of APIs</h2>
+        <img scr="C:/Users/glenn/Downloads/CIT_11200/Program10Languages(zipped)/9.jpg" />
+        <p>
+            A set of defined rules that explain how computers/applications tend to communication with one another.
+        </p>
+
+        <h2>Databases</h2>
+        <img scr="C:/Users/glenn/Downloads/CIT_11200/Program10Languages(zipped)/10.jpg" />
+        <p>
+            An organized collection of structured information or data that's stored in a computer system electronically.
+        </p>
+</body>
+            
+
+    
+
